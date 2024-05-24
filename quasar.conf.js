bbd4c1bcb30bcb5d556a2e245c5a851e9eb92c84
Abas Djumadi <abasd04@gmail.com>
2024-05-25 03:25:09 +0700

    build: setup dotenv

--- quasar.conf.js
+++ quasar.conf.js
@@ -9,6 +9,7 @@
 // https://v1.quasar.dev/quasar-cli/quasar-conf-js
 
 const ESLintPlugin = require("eslint-webpack-plugin");
+require('dotenv').config()
 
 module.exports = function (/* ctx */) {
   return {
@@ -43,9 +44,8 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       env: {
-        SUPABASE_URL: "http://127.0.0.1:54321",
-        SUPABASE_ANON_KEY:
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+        SUPABASE_URL: process.env.SUPABASE_URL,
+        SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
       },
       vueRouterMode: "history", // available values: 'hash', 'history'
 
