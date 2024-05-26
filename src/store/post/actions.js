9c8f4c51c3d10abd0d11f73c997f16a4ed60d8f9
alaraiabdiallah <alaraiabdiallah@gmail.com>
2024-05-26 21:53:02 +0700

    feat: list of post using edge function

--- src/store/post/actions.js
+++ src/store/post/actions.js
@@ -5,30 +5,33 @@ export async function getPosts({ commit }) {
   // set loading
   commit("setLoading", true);
 
-  const { data, error } = await supabase
-  .from("posts")
-  .select("slug, title, description, featured_images, published_at");
-  if (error) {
-    commit("setError", error.message);
-    return false;
+  const supabaseUrl = process.env.SUPABASE_URL;
+  try {
+    const result = await axios.post(`${supabaseUrl}/functions/v1/post-list`, {  }, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.SUPABASE_ANON_KEY}`,
+      },
+    });
+
+    // set posts data
+    commit("setPosts", result.data);
+  } catch (error) {
+    if (error) {
+      commit("setError", error.message);
+      return false;
+    }
+  } finally {
+    // set loading to false
+    commit("setLoading", false);
   }
 
-  // set posts data
-  commit("setPosts", data);
 
-  // set loading to false
-  commit("setLoading", false);
 }
 
 export async function getPostBySlug({ commit, rootState, rootGetters }, slug) {
   // set loading
   commit("setLoading", true);
-  // await new Promise((resolve) => setTimeout(resolve, 10000));
-  // const { data, error } = await supabase
-  //   .from("posts")
-  //   .select("*")
-  //   .eq("slug", slug)
-  //   .single();
 
   const supabaseUrl = process.env.SUPABASE_URL;
 
@@ -42,28 +45,13 @@ export async function getPostBySlug({ commit, rootState, rootGetters }, slug) {
     });
     // handle successful response
     // set post data
-    console.log(result)
     return commit("setPost", result.data);
   } catch (error) {
     // handle error
-    console.log("catch error",error.message)
     commit("setError", error.message);
     return false;
   } finally {
     // set loading to false
     commit("setLoading", false);
   }
-
-
-
-  // if (error) {
-  //   commit("setError", error.message);
-  //   return false;
-  // }
-
-  // // set posts data
-  // commit("setPost", data);
-
-  // // set loading to false
-  // commit("setLoading", false);
 }
